1d42e106df8fab1bf5bae01df4b2f3657c835358
CS18B013 <54976288+CS18B013@users.noreply.github.com>
2021-02-07 19:48:57 +0530

    added menu

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+    
+{
+    public GameObject mainmenu;
+     public void playGame() {
+        SceneManager.LoadScene(1);
+    }
+ 
+ 
+    public void exitGame() {
+        Application.Quit();
+    }
+}
